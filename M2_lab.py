8fa12f75b7a711d0b9cb2685357b2e8964c22c02
Erin Hurm <ehurm@ivytech.edu>
2024-09-01 22:41:27 -0500

    Cleaned up code appearance

--- M2_lab.py
+++ M2_lab.py
@@ -4,8 +4,6 @@ Version: 1.0
 Date: 1 September 2024
 Author: Erin Hurm
 Purpose: Accepts a student's first and last name, checks whether their GPA is high enough for them to be on the Honor Roll or Dean's List.
-
-
 '''
 
 #variables
@@ -14,7 +12,6 @@ firstName = ""
 studentGPA = 0
 
 #constants || dl = dean's list, hr = honor roll
-
 dlGPA = 3.5
 hrGPA = 3.25
 
@@ -23,6 +20,7 @@ lastName = input("Enter the student's last name (or ZZZ to exit): ")
 while not lastName == "ZZZ":
     firstName = input("Enter the student's first name: ")
     studentGPA = float(input("Enter the student's GPA: "))
+    
     if studentGPA >= dlGPA:
         print(f"{firstName} {lastName} has made the Dean's List!")
     elif studentGPA >= hrGPA:
